5aabc76d278ffa7e2042f8f39dd011e2a8b0920c
JosePalenciaa <100240798+JosePalenciaa@users.noreply.github.com>
2022-03-11 14:47:40 +1300

    Add files via upload
    skill issue

--- 03_geting_input.py
+++ 03_geting_input.py
@@ -1,5 +1,5 @@
 # Asl the user for their name
-username = input ("What is your name?")
+username = input ("Hajimemashite, what is your name?")
 
 # Ask the user for their favourite integer
 fav_num = int(input("What is your favourite integer?"))
@@ -10,5 +10,10 @@ half = fav_num / 2
 squared = fav_num * fav_num
 
 # Greet the user
-print("Hajimemashite {}, your favourite number is {}".format(username, fav_num))
+print("Konnichiwa {}, your favourite number is {}".format(username, fav_num))
 
+# Output the results of doubling, halving 
+# and squaring their favourite number
+print("Double {} is {}". format(fav_num, double))
+print("Half {} is {}". format(fav_num, half))
+print("Squared {} is {}". format(fav_num, squared))
