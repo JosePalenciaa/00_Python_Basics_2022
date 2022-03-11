c19884cb7b3ff20eec7af129aab64f54f24746bf
JosePalenciaa <100240798+JosePalenciaa@users.noreply.github.com>
2022-03-11 14:33:00 +1300

    Add files via upload
    Double
    Halve
    Squared

--- 03_geting_input.py
+++ 03_geting_input.py
@@ -3,3 +3,8 @@ username = input ("What is your name?")
 
 # Ask the user for their favourite integer
 fav_num = int(input("What is your favourite integer?"))
+
+# Double, halve and square the number
+double = fav_num * 2
+halve = fav_num / 2 
+square = fav_num * fav_num
