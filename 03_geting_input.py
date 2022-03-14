de776edc622ac20981733c74449811e910e540ad
JosePalenciaa <100240798+JosePalenciaa@users.noreply.github.com>
2022-03-15 11:34:51 +1300

    Add files via upload

--- 03_geting_input.py
+++ 03_geting_input.py
@@ -1,5 +1,5 @@
-# Asl the user for their name
-username = input ("Hajimemashite, what is your name?")
+# Ask the user for their name
+username = input ("Hello user, what is your name?")
 
 # Ask the user for their favourite integer
 fav_num = int(input("What is your favourite integer?"))
@@ -10,10 +10,10 @@ half = fav_num / 2
 squared = fav_num * fav_num
 
 # Greet the user
-print("Konnichiwa {}, your favourite number is {}".format(username, fav_num))
+print("Hello {}, your favourite number is {}".format(username, fav_num))
 
 # Output the results of doubling, halving 
 # and squaring their favourite number
-print("Double {} is {}". format(fav_num, double))
-print("Half {} is {}". format(fav_num, half))
-print("Squared {} is {}". format(fav_num, squared))
+print("The double {} is {}". format(fav_num, double))
+print("Half of {} is {}". format(fav_num, half))
+print("{} squared is {}". format(fav_num, squared))
